4ee442242f57cac48637e3eb27cceede114f4a09
Nailson Boaz Costa Leite <nailsonboaz@gmail.com>
2015-05-19 00:21:48 -0300

    cluster stat and write clusters

--- R/dbscan.R
+++ R/dbscan.R
@@ -1,15 +1,18 @@
 #install.packages("fpc")
+#install.packages("mgcv")
+
 library("fpc")
-install.packages("mgcv")
 library("mgcv")
 
 matrixSimmilarity <- read.delim("~/GitHub/mestrado_UFCG/Java Workspace/DiversificationAlgorithms/data/allItem_Category.tsv")
 matrixSimmilarity = UserMatrixSimmilarity
-matrixSimmilarity = matrixSimmilarity[,c(1:8761)]
+matrixSimmilarity = matrixSimmilarity[,c(1:8723)]
 
 matrixSimmilarity = as.matrix(matrixSimmilarity, )
 
 dissimilarity = 1.0-matrixSimmilarity
+rm(matrixSimmilarity)
+rm(UserMatrixSimmilarity)
 
 hist(matrixSimmilarity[matrixSimmilarity!=0.0])
 
@@ -30,23 +33,31 @@ save(pamkResult, file='pamkResult.rda')
 load(file="C:/Users/Nailson/Dropbox/pamkResult.rda", )
 
 pamkResult
+pamkResult$pamobject$silinfo$clus.avg.widths
 
+clustered_users = data.frame(user=substring(colnames(dissimilarity),2), cluster=pamkResult$pamobject$clustering)
+write.table(clustered_users, quote=F, sep="\t", file="pamk.tsv")
 
 # dbscan : Generates a density based clustering of arbitrary shape
 # input : data, eps, minPoints, scale, method, seeds, showplot, countmode, ...
-results = dbscan(data = dissimilarity, eps=0.15, MinPts= 100, method="dist", showplot=T,)
+results = dbscan(data = dissimilarity, eps=0.25, MinPts= 100, method="dist", showplot=F, )
 results
-dbscanCBI
 
-cluster.stats(d = dissimilarity, clustering= results$cluster)
-
+#  Validate Cluster dbscan
+clusterStatsdbscan = cluster.stats(d = dissimilarity, clustering= results$cluster)
 clustered_users = data.frame(user=substring(colnames(dissimilarity),2), cluster=results$cluster)
 
 boxplot(results)
-
-dbscan(data = dissimilarity, eps=0.14, MinPts= 20, method="dist", showplot=T,)
 
 
 # kmeans
 resultKmeans = kmeans(dissimilarity, centers=8)
 save(resultKmeans, file='resultKmeans.rda')
+load(file="resultKmeans.rda", )
+clustered_users = data.frame(user=substring(colnames(dissimilarity),2), cluster=resultKmeans$cluster)
+write.table(clustered_users, quote=F, sep="\t", file="kmeans.tsv")
+
+# Validate Cluster Kmeans
+resultKmeans$size
+resultKmeans$totss
+clusterStatsKmeans = cluster.stats(d = dissimilarity, clustering= resultKmeans$cluster, silhouette=T)
