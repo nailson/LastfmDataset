88c7687f3ec640277c39e146e78458701f005fbe
Nailson Boaz Costa Leite <nailsonboaz@gmail.com>
2015-06-16 01:15:31 -0300

    not have tag script

---
+++ R/not_have_tag.R
@@ -0,0 +1,18 @@
+
+
+artist_tags <- read.delim("~/GitHub/LastfmDataset/new data/artist relation/artist_tags.tsv", header=F)
+
+dataset <- read.delim("~/GitHub/LastfmDataset/new data/dataset.tsv", header=F)
+
+x = data.frame( V1 = unique(dataset[ !(dataset$V2%in%artist_tags$V1),2]) )
+
+map_artists <- read.delim("~/GitHub/LastfmDataset/new data/map/map_artists.tsv", header=F)
+
+x = merge(x,map_artists, by="V1")
+
+x = x[,c(1,4)]
+y = unique(x[ x$V2!="",])
+write.table(y[,2], file="not_have_tag_mbid.tsv", col.names=F, row.names=F, quote=F, sep="\n")
+
+y = unique(x[ x$V3!="",])
+write.table(y[,3], file="not_have_tag_name.tsv", col.names=F, row.names=F, quote=F, sep="\n")
