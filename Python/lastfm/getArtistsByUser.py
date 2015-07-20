670e42ad00201eb8d60a4ea97c9001e94e8eee7f
Nailson <nailsonboaz@gmail.com>
2015-07-20 09:51:19 -0300

    add files

--- Python/lastfm/getArtistsByUser.py
+++ Python/lastfm/getArtistsByUser.py
@@ -18,7 +18,7 @@ def read_the_dataset(the_dataset_file):
 	with file(the_dataset_file,'r') as infile:
 		f = open('/home/nailson/Documentos/coleta.txt', 'a')
         
-        	f.write("\"userName\"\t\"artistName\"\t\"artistMbid\"\t\"artistUrl\"\t\"playcount\"\t\"rank\"\n")
+        	#f.write("\"userName\"\t\"artistName\"\t\"artistMbid\"\t\"artistUrl\"\t\"playcount\"\t\"rank\"\n")
 
 		for line in infile:   
 			userName = line.rstrip('\n') 
@@ -31,20 +31,29 @@ def getTracks_by_User(userName, f):
 
 	#for i in range(1,totalPages+1):
 	i=1
-	while (i <= 1):
+	while (i <= totalPages):
 
 		page_number = i
-		i += 1
+		
 		user_id = userName
-		string_request = 'http://ws.audioscrobbler.com/2.0/?method=' + method + '&user='+user_id + '&api_key='+ api_key
+		#print(user_id.rstrip('\n'))
+		string_request = 'http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&user='+user_id+'&api_key='+ api_key
 		options = ""
 		options += '&format=json'
 		options += '&limit=' + str(limit_per_page)
 
+		#print(string_request)
 		print(string_request + options + '&page=' + str(page_number))
-		r = requests.get(string_request + options + '&page=' + str(page_number) )
-		
+
+		try:
+			r = requests.get(string_request + options + '&page=' + str(page_number) )
+		except requests.exceptions.ConnectionError:
+			time.sleep(5)
+			continue
+
+		i += 1
 		if(r.text == "" or r.text == '""'):
+			print('EMPTY')
 			continue
 		
 		if 'error' in r.text:
@@ -73,26 +82,35 @@ def getTracks_by_User(userName, f):
 				continue
 		
 			total_elements = jsonResult['topartists']['@attr']['total']
-			totalPages = jsonResult['topartists']['@attr']['totalPages']
-			if totalPages > 2:
-				totalPages = 2
-	
+			totalPages = int(jsonResult['topartists']['@attr']['totalPages'])
+			print(totalPages)
 			#totalPages = 1;
 			#print(jsonResult['toptracks']['track']['name'])
 			for artist in jsonResult['topartists']['artist']:
-				#print('trackName '+track )
-				artistName = str(unicode( artist['name'] ).encode('ascii', 'ignore'))
-				artistPlaycount = artist['playcount']
-				artistMbid = artist['mbid']
-				artistUrl =  str(unicode( artist['url'] ).encode('ascii', 'ignore'))
-				artistRank = artist['@attr']['rank']
+				try:
+					#print('trackName '+track )
+					artistName = str(unicode( artist['name'] ).encode('ascii', 'ignore'))
+					artistPlaycount = artist['playcount']
+					artistMbid = artist['mbid']
+					artistUrl =  str(unicode( artist['url'] ).encode('ascii', 'ignore'))
+					artistRank = artist['@attr']['rank']
 
-				f.write(userName+"\t"+artistName+"\t"+artistMbid+"\t"+artistUrl+"\t"+artistPlaycount+"\t"+artistRank+"\n")
+					f.write(userName+"\t"+artistName+"\t"+artistMbid+"\t"+artistUrl+"\t"+artistPlaycount+"\t"+artistRank+"\n")
 
-			time.sleep(0.1)
+				except(IndexError,TypeError):
+					artistName = str(unicode( jsonResult['topartists']['artist']['name'] ).encode('ascii', 'ignore'))
+					artistPlaycount = jsonResult['topartists']['artist']['playcount']
+					artistMbid = jsonResult['topartists']['artist']['mbid']
+					artistUrl =  str(unicode( jsonResult['topartists']['artist']['url'] ).encode('ascii', 'ignore'))
+					artistRank = jsonResult['topartists']['artist']['@attr']['rank']
+
+					f.write(userName+"\t"+artistName+"\t"+artistMbid+"\t"+artistUrl+"\t"+artistPlaycount+"\t"+artistRank+"\n")
+					continue
+
+			time.sleep(0.4)
 
 		else:
-			time.sleep(10)
+			time.sleep(2)
 		
         
 
@@ -105,7 +123,7 @@ if __name__ == "__main__":
 
 	# Read the _empty file (the task)
     
-	read_the_dataset('all_usernames.tsv')
+	read_the_dataset('all_usernames2.tsv')
 	#getTracks_by_User('dr')
 
 	print 'done.'
